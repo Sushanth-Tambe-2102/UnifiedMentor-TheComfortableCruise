b87a2a70c3a9f45e4e53672c96e67211422f8069
sushanthtambe <sushanth.tambe@gmail.com>
2024-01-08 12:34:13 +0530

    Project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import React from 'react';
+
+function App() {
+  
+
+  return (
+    
+      
+          <div>TheComfortableCruise</div>
+       
+    
+  )
+}
+
+export default App
