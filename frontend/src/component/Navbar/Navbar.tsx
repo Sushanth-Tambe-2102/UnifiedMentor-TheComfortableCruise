cfa9a38a69560592aa2030567009f8a474958847
sushanthtambe <sushanth.tambe@gmail.com>
2024-01-15 03:08:38 +0530

    Navbar changes

--- frontend/src/component/Navbar/Navbar.tsx
+++ frontend/src/component/Navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 import shipLogo from '/logoFinal.png'
 import styled from '@emotion/styled'
 import { Box, Typography,IconButton } from '@mui/material'
-
+import LoginIcon from '@mui/icons-material/Login';
 
 const Navbar = () => {
     
 const NavBox = styled(Box)({   
     width: '100%',
-    background: `linear-gradient(60deg,rgba(0,59,97,1)  0%, rgba(4,105,151,1) 30%,  rgba(49,141,178,1) 50%,rgba(55,175,247,1) 70%, rgba(48,177,206,1) 100%)`,
+    background: `linear-gradient(60deg,rgba(0,59,97,1)  0% , rgba(4,105,151,1) 30%,  rgba(49,141,178,1) 50%,rgba(55,175,247,1) 70%, rgba(48,177,206,1) 100%)`,
     color: 'white',
     display:'flex',
     flexDirection: 'row',
@@ -44,7 +44,13 @@ const Waves=styled(Box)({
   const LoginBox =styled(Box)({
     position: 'absolute',
     right: '30px',
-    zIndex:1
+    color: "rgba(0,172,193,1)",
+    zIndex:1,
+    "&:hover": {
+      backgroundColor: "rgba(84,58,183,0)",
+      color:'white',
+
+    },
   })
   const LoginButton = styled(IconButton)({
     
@@ -54,20 +60,34 @@ const Waves=styled(Box)({
     width: "160px",
     display: "flex",
     justifyContent: "start",
-    padding: "0px 0px 0px 20px ",
+    padding: "0px 0px 0px 30px ",
     height: "50px",
     fontFamily: "sans-serif",
     fontSize: "22px",
     border:'2px solid white',
     color: "rgba(0,172,193,1)",
-    zIndex:2,
-  
     "&:hover": {
       backgroundColor: "rgba(84,58,183,0)",
       color:'white',
 
     },
+    zIndex:2,
+  
+    
   });
+  const Loginicon =styled(LoginIcon)({
+    fontSize: "22px",
+    position: "absolute",
+    right:'30px',
+    "&:hover": {
+      backgroundColor: "rgba(84,58,183,0)",
+      color:'white',
+
+    },
+
+    zIndex:2
+  })
+  
   
   return (
     <>
@@ -81,9 +101,10 @@ const Waves=styled(Box)({
           The Comfortable Cruise
           </NavHeader>
           <LoginBox>
-        <LoginButton>
-          Login
+        <LoginButton>Login
+          <Loginicon/>
         </LoginButton>
+        
         </LoginBox>
         <Waves>
         {/* <svg
